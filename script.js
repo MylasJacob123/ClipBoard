f892cabcd93b19a2db17a0166d3a75ec009fefb4
Loneastro95 <ayandasontlaba6@gmail.com>
2024-04-30 13:14:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const head1 = document.getElementById("head1");
+const para01 = document.getElementById("para01");
+const head2 = document.getElementById("head2");
+const para02 = document.getElementById("para02");
+const TP001 = document.getElementById("TP001");
+const p001 = document.getElementById("p001");
+const TP002 = document.getElementById("TP002");
+const p002 = document.getElementById("p002");
+const TP003 = document.getElementById("TP003");
+const p003 = document.getElementById("p003");
+const head3 = document.getElementById("head3");
+const para03 = document.getElementById("para03");
+const head4 = document.getElementById("head4");
+const para04 = document.getElementById("para04");
+const TP004 = document.getElementById("TP004");
+const p004 = document.getElementById("p004");
+const TP005 = document.getElementById("TP005");
+const p005 = document.getElementById("p005");
+const TP006 = document.getElementById("TP006");
+const p006 = document.getElementById("p006");
+const head5 = document.getElementById("head5");
+const para05 = document.getElementById("para05");
+
+
+function info(){
+    head1.textContent = "A history of everything you copy";
+    para01.textContent ="Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.";
+    head2.textContent = "Keep track of your snippets";
+    para02.textContent = "Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.";
+    TP001.textContent = "Quick Search";
+    p001.textContent = "Easily search your snippets by content, category, web address, application, and more.";
+    TP002.textContent = "iCloud Sync";
+    p002.textContent = "Instantly saves and syncs snippets across all your devices.";
+    TP003.textContent = "Complete History";
+    p003.textContent = " Retrieve any snippets from the first moment you started using the app.";
+    head3.textContent =  "Access Clipboard anywhere";
+    para03.textContent = " Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.";
+    head4.textContent = "Supercharge your workflow";
+    para04.textContent = "We’ve got the tools to boost your productivity.";
+    TP004.textContent = "Create blacklists";
+    p004.textContent = "Ensure sensitive information never makes its way to your clipboard by excluding certain sources." ;
+    TP005.textContent = "Plain text snippets";
+    p005.textContent = " Remove unwanted formatting from copied text for a consistent look.";
+    TP006.textContent = "Sneak preview";
+    p006.textContent = "Quick preview of all snippets on your Clipboard for easy access.";
+    head5.textContent = "Clipboard for iOS and Mac OS";
+    para05.textContent = " Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you’re ready to start adding to your clipboard.";
+    
+}
+info();
